ac449f7e1d431f6e9dd8b01e2fc0eb5d4ec21d4c
feiy <feiy@163.com>
2019-10-22 18:14:49 +0800

    唔该

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ module.exports = {
     // { src: '@/plugins/swiper', ssr: false }
   ],
   server: {
-    host: '192.168.48.150',
+    // host: '192.168.48.150',
     port: 3001, // default: 3000
   },
   /*
